836cd44f2a8a359c513936f84c7e8c63f7afaf1c
Anmol Kumar <anmol4979199@gmail.com>
2023-07-16 12:53:06 +0530

    .

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var nodemailer = require('nodemailer');
+const moment = require('moment');
+require('moment-timezone');
+const ejs = require('ejs');
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://anmol4979199:anmoltest123@test.8oysegr.mongodb.net/TLBooking');
@@ -22,9 +26,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/sign_up', function (req, res) {
-    var name = req.body.name;
-    var start = new Date(req.body.start);
-    var end = new Date(req.body.end);
+    var name = req.body.user;
+    var start = new Date(req.body.utcStartTime);
+    var end = new Date(req.body.utcEndTime);
     var status = req.body.status;
 
     const query = {
@@ -68,22 +72,43 @@ app.post('/sign_up', function (req, res) {
                     pass: 'kopl--==kopl'
                 }
             });
+           
             const sub = `Confirmed Booking for ${data.user}` ;
-            const txt = `Dear ${data.user},\n\nYour Booking is from (${data.startTime}) to (${data.endTime}).\n\nBest regards,\nThe TL Team`;
-            var mailOptions = {
-                from: '"Laser Cutting Confirmatiom" <anmol4979199@gmail.com>',
-                to: data.user,
-                subject: sub,
-                text: txt
-            };
+            const mailStartTime = moment.utc(data.startTime).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+            const mailEndTime = moment.utc(data.endTime).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+            const txt = `Dear <b>${data.user}</b>,<br><br>We hereby confirm your Laser Cutting Request. The timings are as follwing:<br> From<b> ${mailStartTime} <br> </b>to <b> ${mailEndTime}</b>.<br><br>Best regards,<br>The TL Team`;
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
+            // async..await is not allowed in global scope, must use a wrapper
+            async function main() {
+                const html = await ejs.renderFile('views/email.ejs', { name, mailStartTime, mailEndTime, });
+                // send mail with defined transport object
+                const info = await transporter.sendMail({
+                    from: '"Laser Cutting Confirmation" <anmol4979199@gmail.com>',
+                    to: data.user,
+                    subject: sub,
+                    html: html
+                });
+
+                console.log("Message sent: %s", info.messageId);
+              
+            }
+            main().catch(console.error);
+            // var mailOptions = {
+            //     from: '"Laser Cutting Confirmatiom" <anmol4979199@gmail.com>',
+            //     to: data.user,
+            //     subject: sub,
+            //     text: txt
+            // };
+
+            // await new Promise((resolve, reject) => {
+            // transporter.sendMail(mailOptions, function (error, info) {
+            //     if (error) {
+            //         console.log(error);
+            //     } else {
+            //         console.log('Email sent: ' + info.response);
+            //     }
+            // });
+            // });
             return res.send('Confirmed!! You will get a mail soon!');
             };
     })
@@ -97,11 +122,22 @@ app.get('/', function (req, res) {
     res.render('form')
 }).listen(3000)
 
-
+app.get('/new', function(req,res){
+    res.render('new')
+})
+app.get('/mail', function(req,res){
+    res.render('email')
+})
 app.get('/documents', function (req, res) {
     
    db.collection('LaserCutting').find({}).toArray().then((ans) => {
-       res.render('documents',{ ans: ans})
+       const convertedDates = ans.map((doc) => {
+           const startTime = moment.utc(doc.startTime).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+           const endTime = moment.utc(doc.endTime).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+           return { ...doc, startTime, endTime };
+       });
+
+       res.render('documents', { ans: convertedDates })
     });
 })
 
